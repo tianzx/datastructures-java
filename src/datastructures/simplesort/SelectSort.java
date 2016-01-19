1a2c3ae0e1db0a71dff1639667e9ff44ad14ee73
tianzx <436011080@qq.com>
2016-01-19 21:12:29 +0800

    selectSort

--- src/datastructures/simplesort/SelectSort.java
+++ src/datastructures/simplesort/SelectSort.java
@@ -7,6 +7,26 @@ public class SelectSort {
 		for(int i=0;i<as.length-1;i++) {
 			//init the min position
 			min  = i;
+			for(int j=i+1;j<as.length;j++) {
+				if(as[min]>as[j]) {
+					min = j;
+				}
+			}
+			swap(as,min,i);
 		}
+	}
+	
+	private void swap(int[]as ,int aIndex, int bIndex) {
+		int temp = as[aIndex];
+		as[aIndex] = as[bIndex];
+		as[bIndex] = temp;
+	}
+	
+	public static void main(String[] args) {
+		SelectSort ss = new SelectSort();
+		int[] as = {1,4,12,8,2,6,9,5,10};
+		ss.selectSort(as);
+		for(int i :as )
+			System.err.print(i+" ");
 	}
 }
