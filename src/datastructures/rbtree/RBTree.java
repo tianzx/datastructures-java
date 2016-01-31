e5fe42b4b58d7d834f500e196f315222d4155725
tianzx <436011080@qq.com>
2016-01-31 22:05:24 +0800

    finish

--- src/datastructures/rbtree/RBTree.java
+++ src/datastructures/rbtree/RBTree.java
@@ -1,6 +1,6 @@
 package datastructures.rbtree;
 /***
- * show how to operate BT
+ * show how to operate RB
  * @author tianzx
  *
  */
@@ -9,6 +9,57 @@ public class RBTree {
 	 * root RBNode
 	 */
 	private RBNode root;
+
+	/**********************************************************/
+	private void afterInsert(RBNode nowNode){
+		//1:if root
+		if(nowNode.getParent()==null){
+			nowNode.setRed(false);
+			this.root = nowNode;
+		}else if(!nowNode.getParent().isRed()){
+		//2:if nowNode parent is black
+		}else if(nowNode.getParent().isRed()){
+			RBNode g = nowNode.getParent().getParent();
+			RBNode u =null;
+			if(g!=null){
+				u = g.getLeftChild()==nowNode.getParent()?g.getRightChild():g.getLeftChild();
+			}
+			//3:
+			if(u!=null&&u.isRed()) {
+				g.setRed(true);
+				u.setRed(false);
+				nowNode.getParent().setRed(false);
+				nowNode = g;
+				this.afterInsert(nowNode);
+			}else if((u==null|| !u.isRed())&&nowNode==nowNode.getParent().getLeftChild()&&nowNode.getParent() ==g.getLeftChild()){
+				nowNode.getParent().setRed(false);
+				g.setRed(true);
+				this.rightRotate(g);
+				this.afterInsert(nowNode);
+			}else if((u==null|| !u.isRed())&&nowNode==nowNode.getParent().getRightChild()&&nowNode.getParent() ==g.getRightChild()){
+				nowNode.getParent().setRed(false);
+				g.setRed(true);
+				this.leftRotate(g);
+				this.afterInsert(nowNode);
+			}else if(((u==null|| !u.isRed())&&nowNode==nowNode.getParent().getRightChild()&&nowNode.getParent() ==g.getLeftChild())){
+				RBNode oldParent = nowNode.getParent();
+				this.leftRotate(oldParent);
+				this.afterInsert(oldParent);
+			}else if(((u==null|| !u.isRed())&&nowNode==nowNode.getParent().getLeftChild()&&nowNode.getParent() ==g.getRightChild())) {
+				RBNode oldParent = nowNode.getParent();
+				this.rightRotate(oldParent);
+				this.afterInsert(oldParent);
+			}
+		}
+	}
+	
+	private void rightRotate(RBNode node){
+		
+	}
+	private void leftRotate(RBNode node){
+		
+	}
+	/**********************************************************/
 	/**
 	 * find a RBNode 
 	 * @param key id
@@ -63,8 +114,10 @@ public class RBTree {
 			
 		}
 	
-		
-		
+		/**
+		 * after insert ,start adjust
+		 */
+		this.afterInsert(newRBNode);
 	}
 	/**
 	 * 
